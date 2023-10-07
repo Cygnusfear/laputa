18913e123cd71ff3808a9ee05fd051a0973bb36f
Sing <16409791+singyiu@users.noreply.github.com>
2023-10-06 23:08:12 -0700

    feat: adding position related mud tables

--- packages/contracts/mud.config.ts
+++ packages/contracts/mud.config.ts
@@ -1,4 +1,4 @@
-import { mudConfig } from "@latticexyz/world/register";
+import { mudConfig, resolveTableId } from "@latticexyz/world/register";
 
 export default mudConfig({
   tables: {
@@ -6,5 +6,43 @@ export default mudConfig({
       keySchema: {},
       valueSchema: "uint32",
     },
+    Position: {
+      valueSchema: {
+        x: "int32",
+        y: "int32",
+        z: "int32",
+      },
+    },
+    Size: {
+      valueSchema: {
+        width: "uint32",
+        length: "uint32",
+        height: "uint32",
+      },
+    },
+    Orientation: {
+      valueSchema: {
+        yaw: "uint32",
+        pitch: "uint32",
+        roll: "uint32",
+      },
+    },
+    EntityType: {
+      valueSchema: {
+        typeId: "uint32",
+      },
+    },
   },
+  modules: [
+    {
+      name: "KeysInTableModule",
+      root: true,
+      args: [resolveTableId("Position")],
+    },
+    {
+      name: "KeysWithValueModule",
+      root: true,
+      args: [resolveTableId("Position")],
+    },
+  ],  
 });
