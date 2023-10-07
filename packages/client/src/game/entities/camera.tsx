93fed8809b94c1b0286e176fb3ab974f573968dd
Alexander Mangel <cygnusfear@gmail.com>
2023-10-07 11:08:54 -0500

    feat: orbitcontrols rotation

--- packages/client/src/game/entities/camera.tsx
+++ packages/client/src/game/entities/camera.tsx
@@ -1,8 +1,32 @@
-import { useRef } from "react";
-import { OrbitControls, PerspectiveCamera } from "@react-three/drei";
+import { useEffect, useRef } from "react";
+import {
+  OrbitControls,
+  OrbitControlsProps,
+  PerspectiveCamera,
+} from "@react-three/drei";
 
 function Camera() {
-  const orbitRef = useRef();
+  const orbitRef = useRef<OrbitControlsProps>(null!);
+
+  const setCameraAngle = () => {
+    if (orbitRef && orbitRef.current !== null) {
+      // @ts-ignore
+      orbitRef.current.setPolarAngle(Math.PI / 3.5);
+      orbitRef.current.maxPolarAngle = Infinity;
+    }
+  };
+
+  useEffect(() => {
+    setCameraAngle();
+  }, [orbitRef]);
+
+  useEffect(() => {
+    window.addEventListener("gameLoaded", setCameraAngle);
+    return () => {
+      window.removeEventListener("gameLoaded", setCameraAngle);
+    };
+  }, []);
+
   return (
     <>
       <PerspectiveCamera position={[0, 50, 50]} fov={35} />
