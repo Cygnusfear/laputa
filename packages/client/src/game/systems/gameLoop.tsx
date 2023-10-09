4b3985183f7f14566d04429b714a85f4d5a17f74
Alexander Mangel <cygnusfear@gmail.com>
2023-10-09 11:43:19 -0500

    fix: just add on spawn

--- packages/client/src/game/systems/gameLoop.tsx
+++ packages/client/src/game/systems/gameLoop.tsx
@@ -1,25 +1,15 @@
 import { useEffect } from "react";
 import resourceFactory from "./resourceFactory";
 import EntityData from "../data/entities";
-import { useStore } from "../store";
 
 function GameLoop() {
-  const {
-    world: { entities },
-  } = useStore();
   useEffect(() => {
-    setInterval(() => {
-      console.log("Game loop");
-      if (
-        entities.filter((entity) => entity.entityType === "resource")?.length <
-        10
-      ) {
-        const { createResource, randomEmptyPosition } = resourceFactory();
-        const position = randomEmptyPosition();
-        createResource(EntityData.resources.crystalFloat, position!);
-      }
-    }, 5000);
-  }, [entities]);
+    for (let i = 0; i < 10; i++) {
+      const { createResource, randomEmptyPosition } = resourceFactory();
+      const position = randomEmptyPosition();
+      createResource(EntityData.resources.crystalFloat, position!);
+    }
+  }, []);
 
   return <></>;
 }
