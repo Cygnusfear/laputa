7e5002bb079e82c47a925d4082c8d38e9142f66c
Alexander Mangel <cygnusfear@gmail.com>
2023-10-16 12:40:48 -0500

    style: color picker styling

--- packages/client/src/components/ui/colorWheel.tsx
+++ packages/client/src/components/ui/colorWheel.tsx
@@ -1,10 +1,11 @@
 import { useState } from "react";
 import Wheel from "@uiw/react-color-wheel";
-import { ColorResult } from "@uiw/color-convert";
+import { ColorResult, hsvaToHex } from "@uiw/color-convert";
 import { useStore } from "@/game/store";
 
 function ColorWheel() {
   const [hsva, setHsva] = useState({ h: 214, s: 43, v: 90, a: 1 });
+  const [hex, setHex] = useState(hsvaToHex({ h: 214, s: 43, v: 90, a: 1 }));
   const {
     input: {
       cursor: { setCursor },
@@ -13,20 +14,28 @@ function ColorWheel() {
 
   const setColor = (color: ColorResult) => {
     console.log(color.hex);
+    setHex(color.hex);
     setCursor({ color: color.hex });
     setHsva({ ...hsva, ...color.hsva });
   };
 
   return (
-    <div className="card">
-      <div className="card-wheel flex items-center">
-        <Wheel
-          color={hsva}
-          onChange={setColor}
-          className=""
-          width={70}
-          height={70}
-        />
+    <div className="card m-2">
+      <div className=" card-wrapper">
+        <div
+          className="card-wheel flex items-center"
+          style={{ backgroundColor: hex }}
+        >
+          <div className="mx-auto" style={{ width: "70px", height: "70px" }}>
+            <Wheel
+              color={hsva}
+              onChange={setColor}
+              className="flex"
+              width={70}
+              height={70}
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
