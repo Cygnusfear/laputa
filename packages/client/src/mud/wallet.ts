be53d0ae8e2c4fcac84ca6cfcb0abcd67de38c2d
Alexander Mangel <cygnusfear@gmail.com>
2023-10-16 07:12:06 -0500

    feat: external wallet with switch chain
    Signed-off-by: Alexander Mangel <cygnusfear@gmail.com>

--- packages/client/src/mud/wallet.ts
+++ packages/client/src/mud/wallet.ts
@@ -7,6 +7,7 @@ import {
   custom,
   // custom,
   Hex,
+  toHex,
   // http,
   Transport,
   WalletClient,
@@ -79,39 +80,16 @@ export async function setupWallet(
   //   return walletClient as TWalletClient;
   // }
 
+  // check for potential extension wallets
   if ("ethereum" in window) {
-    try {
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      const extensionWallet = window.ethereum as any;
-      const walletClient = createWalletClient({
-        ...clientOptions,
-        transport: custom(extensionWallet),
-      });
-      const accounts = await walletClient.requestAddresses();
-      console.log(extensionWallet, walletClient);
-      if (walletClient.chain!.id === networkConfig.chainId) {
-        console.log(
-          `Connected to extension wallet -> ChainID ${networkConfig.chainId}`
-        );
-        walletClient.account = {
-          address: accounts[0] as Hex,
-          type: "json-rpc",
-        };
-        return walletClient as TWalletClient;
-      }
-      console.error("Wallet does not have the correct ChainID");
-    } catch (error) {
-      console.error("Error connecting to extension wallet: ", error);
-    }
+    return (await connectExtensionWallet(
+      networkConfig,
+      clientOptions
+    )) as TWalletClient;
   }
 
   // if they haven't we consider this a new player and we create a burner wallet
-  const account = await createBurnerWallet(networkConfig);
-  const walletClient = createWalletClient({
-    account: account,
-    ...clientOptions,
-  });
-  return walletClient as TWalletClient;
+  return await createBurnerWallet(networkConfig, clientOptions);
 }
 
 /**
@@ -120,11 +98,103 @@ export async function setupWallet(
  * @param {ClientConfig} clientOptions - The client configuration object.
  * @returns {Promise<TWalletClient>} A promise that resolves to a wallet client.
  */
-export async function createBurnerWallet(networkConfig: NetworkConfig) {
+export async function createBurnerWallet(
+  networkConfig: NetworkConfig,
+  clientOptions: ClientConfig
+) {
   /*
    * Create a temporary wallet and a viem client for it
    * (see https://viem.sh/docs/clients/wallet.html).
    */
+  const account = await createBurnerAccount(networkConfig.privateKey as Hex);
+  const walletClient = createWalletClient({
+    account: account,
+    ...clientOptions,
+  });
+  return walletClient as TWalletClient;
+}
 
-  return createBurnerAccount(networkConfig.privateKey as Hex);
+/**
+ * Connects to an extension wallet with the specified network configuration and client options.
+ * @param {NetworkConfig} networkConfig - The network configuration object.
+ * @param {ClientConfig} clientOptions - The client configuration object.
+ * @returns {Promise<TWalletClient>} A promise that resolves to a wallet client.
+ * @throws {Error} If the wallet can't connect.
+ */
+export async function connectExtensionWallet(
+  networkConfig: NetworkConfig,
+  clientOptions: ClientConfig
+) {
+  try {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const extensionWallet = (window as any).ethereum;
+    if (extensionWallet.chainId !== toHex(networkConfig.chainId)) {
+      try {
+        // Do initial request (if chain exists in wallet)
+        await extensionWallet.request({
+          method: "wallet_switchEthereumChain",
+          params: [{ chainId: toHex(networkConfig.chain.id) }],
+        });
+      } catch (switchError) {
+        // This error code indicates that the chain has not been added to MetaMask.
+        // @ts-ignore
+        if (switchError.code === 4902) {
+          try {
+            // Add wallet to Extension Wallet
+            await extensionWallet.request({
+              method: "wallet_addEthereumChain",
+              params: [
+                {
+                  chainId: toHex(networkConfig.chainId),
+                  chainName: networkConfig.chain.name,
+                  rpcUrls: networkConfig.chain.rpcUrls,
+                },
+              ],
+            });
+          } catch (addError) {
+            console.error(addError);
+          }
+        }
+      }
+    }
+
+    // Setting up the Viem client
+    const walletClient = createWalletClient({
+      ...clientOptions,
+      transport: custom(extensionWallet),
+    });
+    const accounts = await walletClient.requestAddresses();
+
+    // Eventually connect to the chain and return
+    const connectToChain = () => {
+      console.log(
+        `Connected to extension wallet -> ChainID ${networkConfig.chainId}`
+      );
+      walletClient.account = {
+        address: accounts[0] as Hex,
+        type: "json-rpc",
+      };
+      return walletClient as TWalletClient;
+    };
+
+    if (extensionWallet.chainId === toHex(networkConfig.chainId)) {
+      return connectToChain();
+    } else {
+      try {
+        // Wait for the chain changed event before connecting otherwise it fails
+        let chainChanged = false;
+        extensionWallet.on("chainChanged", () => (chainChanged = true));
+
+        // 💀 deathloop
+        while (!chainChanged) {
+          await new Promise((resolve) => setTimeout(resolve, 1000));
+        }
+        return connectToChain();
+      } catch (e) {
+        throw new Error("Wallet does not have the correct ChainID");
+      }
+    }
+  } catch (error) {
+    console.error("Error connecting to extension wallet: ", error);
+  }
 }
