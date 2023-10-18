009151d1b843bb7bd34afb77a6479dd5a5d5ecc3
Sing <16409791+singyiu@users.noreply.github.com>
2023-10-18 16:49:14 -0700

    Revert "Sing/feat deploypolygon 01"

--- packages/contracts/test/CounterTest.t.sol
+++ packages/contracts/test/CounterTest.t.sol
@@ -27,12 +27,12 @@ contract CounterTest is MudTest {
 
   function testCounter() public {
     // Expect the counter to be 1 because it was incremented in the PostDeploy script.
-    uint32 counter = Counter.get();
+    uint32 counter = Counter.get(world);
     assertEq(counter, 1);
 
     // Expect the counter to be 2 after calling increment.
     world.increment();
-    counter = Counter.get();
+    counter = Counter.get(world);
     assertEq(counter, 2);
   }
 }
